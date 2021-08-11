31bf9df47838cd256400003ddbbc2d2140573bee
Romain Dalichamp <romain.dalichamp@gmail.com>
2021-08-10 22:51:48 -0400

    PubSub Component and Service

---
+++ src/app/pubsub/pubsub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pubsub',
+  templateUrl: './pubsub.component.html',
+  styleUrls: ['./pubsub.component.scss']
+})
+export class PubsubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
