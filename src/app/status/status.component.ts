e12ee489fc80c5c679abea04a1ccee2aead4c2ba
Romain Dalichamp <romain.dalichamp@gmail.com>
2021-08-14 21:35:32 -0400

    try to identify memory lead on heroku

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,21 +1,21 @@
-import {Component} from '@angular/core';
-import {Observable} from "rxjs";
-import {RxStompService} from "@stomp/ng2-stompjs";
-import {RxStompState} from "@stomp/rx-stomp";
-import {map} from "rxjs/operators";
-
-@Component({
-  selector: 'app-status',
-  templateUrl: './status.component.html',
-  styleUrls: ['./status.component.scss']
-})
-export class StatusComponent {
-  public connectionStatus$: Observable<string>;
-
-  constructor(public rxStompService: RxStompService) {
-    this.connectionStatus$ = rxStompService.connectionState$.pipe(map((state) => {
-      // convert numeric RxStompState to string
-      return RxStompState[state];
-    }));
-  }
-}
+// import {Component} from '@angular/core';
+// import {Observable} from "rxjs";
+// import {RxStompService} from "@stomp/ng2-stompjs";
+// import {RxStompState} from "@stomp/rx-stomp";
+// import {map} from "rxjs/operators";
+//
+// @Component({
+//   selector: 'app-status',
+//   templateUrl: './status.component.html',
+//   styleUrls: ['./status.component.scss']
+// })
+// export class StatusComponent {
+//   public connectionStatus$: Observable<string>;
+//
+//   constructor(public rxStompService: RxStompService) {
+//     this.connectionStatus$ = rxStompService.connectionState$.pipe(map((state) => {
+//       // convert numeric RxStompState to string
+//       return RxStompState[state];
+//     }));
+//   }
+// }
