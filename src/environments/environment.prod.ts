00e1839615013f9bca495c182079bba47f6eed23
Romain Dalichamp <romain.dalichamp@gmail.com>
2021-08-14 20:38:16 -0400

    websocket url correction

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  wsTwitchBrotherBack: 'ws://twitch-brother-back.herokuapp.com//ws'
+  wsTwitchBrotherBack: 'ws://twitch-brother-back.herokuapp.com/ws'
 };
